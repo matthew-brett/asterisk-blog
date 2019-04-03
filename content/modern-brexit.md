781eaa36749d9096a60fe4ed2d0037f75c2aa23f
Matthew Brett <matthew.brett@gmail.com>
2019-04-03 13:55:52 +0100

    Move to pandoc formatting

---
+++ content/modern-brexit.md
@@ -0,0 +1,19 @@
+title: Brexit and modern art
+date: 2019-04-03 13:40
+tags: politics, brexit, tory, poor education
+category: politics
+slug: modern-brexit
+author: Matthew Brett
+summary: Brexit and modern art
+
+> One senior Tory said [Theresa May, the British prime minister]
+> is "making an art form of bad misjudgements - this is not just
+> a Reubens or a Van Gogh, it's the whole Tate Modern". - [Brexit: May chooses a deal over party unity](https://www.bbc.co.uk/news/uk-politics-47796013)
+
+The Tate Modern exhibits art dating from 1900.  You could
+just imagine [Van
+Gogh](https://en.wikipedia.org/wiki/Vincent_van_Gogh) getting in
+under the wire, although he died in 1890.  The "Senior Tory" does
+not appear to know anything about
+[Reubens](https://en.wikipedia.org/wiki/Peter_Paul_Rubens); he
+died in 1640.
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,31 +2,56 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = u'Matthew Brett'
-SITENAME = u'Asterisk'
-SITEURL = 'http://asterisk.dynevor.org'
+AUTHOR = 'Matthew Brett'
+SITENAME = 'Asterisk'
+SITEURL = ''
 
-TIMEZONE = 'America/Los_Angeles'
+PATH = 'content'
 
-DEFAULT_LANG = u'en'
+TIMEZONE = 'Europe/London'
+
+DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('My pages', 'http://matthew.dynevor.org'),
-          ('Nipy', 'http://nipy.org/'))
+LINKS =  (
+    ('Personal pages', 'http://matthew.dynevor.org'),
+    ('Work pages',
+     'https://www.birmingham.ac.uk/staff/profiles/psychology/brett-matthew.aspx'),
+    ('Nipy', 'http://nipy.org/'),
+    ('GitHub', 'https://github.com/matthew-brett'),
+)
 
 # Social widget
-# SOCIAL = (('You can add links in your config file', '#'),
-#           ('Another social link', '#'),)
 SOCIAL = (())
 
 DEFAULT_PAGINATION = 10
 
+STATIC_PATHS = ['images', 'extra']
+
+EXTRA_PATH_METADATA = {
+    'extra/robots.txt': {'path': 'robots.txt'},
+    'extra/favicon.ico': {'path': 'favicon.ico'},
+    'extra/favicon.ico': {'path': 'favicon.png'},
+}
+
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+# Content caching for faster builds
+CACHE_CONTENT = True
+LOAD_CONTENT_CACHE = True
+CHECK_MODIFIED_METHOD = 'md5'
+CONTENT_CACHING_LAYER = 'reader'
+
+PLUGIN_PATHS = ['plugins/pelican_pandoc_reader']
+PLUGINS = ['pelican_pandoc_reader']
+PANDOC_ARGS = ['--no-highlight',  # use highlight.js instead
+               '--section-divs',  # wrap heading-blocks with <section>
+               '--filter', 'pandoc-citeproc']
